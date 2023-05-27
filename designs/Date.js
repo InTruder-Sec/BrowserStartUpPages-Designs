349c99f489efe4f0993e74352d8de0fbb1b27a84
= <dhakatedeep909@gmail.com>
2023-05-27 21:56:06 +0530

    Update

---
+++ designs/Date.js
@@ -0,0 +1,50 @@
+// Get time
+
+const weekDay = (day) => {
+  switch (day) {
+    case 0:
+      return "Sunday";
+    case 1:
+      return "Monday";
+    case 2:
+      return "Tuesday";
+    case 3:
+      return "Wednesday";
+    case 4:
+      return "Thursday";
+    case 5:
+      return "Friday";
+    case 6:
+      return "Saturday";
+  }
+};
+
+const formatTime = (hours) => {
+  if (hours > 12) {
+    hours = hours - 12;
+  }
+  if (hours === 0) {
+    hours = "00";
+  }
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
+  return hours;
+};
+
+const date = new Date();
+
+const hour = formatTime(date.getHours());
+const min = date.getMinutes();
+const time = `${hour} : ${min}`;
+const day = weekDay(date.getDay());
+const month = date.getMonth();
+const today = date.getDate();
+
+function GetTimeData() {
+  return {
+    date: today,
+    time: time,
+    day: day,
+  };
+}
--- designs/milkyway/script.js
+++ designs/milkyway/script.js
@@ -1,47 +1,7 @@
-// Get time
-
-const weekDay = (day) => {
-  switch (day) {
-    case 0:
-      return "Sunday";
-    case 1:
-      return "Monday";
-    case 2:
-      return "Tuesday";
-    case 3:
-      return "Wednesday";
-    case 4:
-      return "Thursday";
-    case 5:
-      return "Friday";
-    case 6:
-      return "Saturday";
-  }
+const UpdateTime = () => {
+  data = GetTimeData();
+  document.getElementById("time--hours").innerText = data.time;
+  document.getElementById("time--day").innerText = data.day;
 };
 
-const formatTime = (hours) => {
-  if (hours > 12) {
-    hours = hours - 12;
-  }
-  if (hours === 0) {
-    hours = "00";
-  }
-  if (hours < 10) {
-    hours = "0" + hours;
-  }
-  return hours;
-};
-
-const date = new Date();
-
-const hour = formatTime(date.getHours());
-const min = date.getMinutes();
-const day = weekDay(date.getDay());
-const month = date.getMonth();
-const today = date.getDate();
-
-const Main = () => {
-  document.getElementById("time--hours").innerText = `${hour} : ${min}`;
-};
-
-Main();
+setInterval(UpdateTime, 1000);
